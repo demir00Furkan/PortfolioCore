97b46c0d80f8ea294d4788498d2392ecf3f25863
furkan demir <demir.dmr42@gmail.com>
2025-04-09 03:36:25 +0300

    .

---
+++ PortfolioCore/Controllers/AdminLayoutController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioCore.Controllers
+{
+    public class AdminLayoutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public PartialViewResult partialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult partialSidebar()
+        {
+            return PartialView();
+        }
+        public PartialViewResult partialScript()
+        {
+            return PartialView();
+        }
+    }
+}
