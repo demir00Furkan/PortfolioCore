2f21b0ad25504e7638e17332c98eecfd3b217474
furkan demir <demir.dmr42@gmail.com>
2025-04-15 22:22:40 +0300

    ...

---
+++ PortfolioCore/Controllers/ExperienceController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PortfolioCore.Context;
+using PortfolioCore.Entities;
+
+namespace PortfolioCore.Controllers
+{
+    public class ExperienceController : Controller
+    {
+        PortfolioContext context = new PortfolioContext();
+        public IActionResult ExperienceList()
+        {
+            var values =  context.Experiences.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public IActionResult createExperience()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult CreateExperience(Experience experience)
+        {
+            context.Experiences.Add(experience);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceListAction");
+        }
+        public IActionResult DeleteExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            context.Experiences.Remove(value);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+        [HttpGet]
+        public IActionResult UpdateExperience(int id)
+        {
+            var value = context.Experiences.Find(id);
+            return View(value);
+            
+        }
+        [HttpPost]
+        public IActionResult UpdateExperience(Experience x)
+        {
+            context.Experiences.Update(x);
+            context.SaveChanges();
+            return RedirectToAction("ExperienceList");
+        }
+    }
+}
